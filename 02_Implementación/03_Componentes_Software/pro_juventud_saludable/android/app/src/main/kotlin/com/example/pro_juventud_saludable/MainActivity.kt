d6cee541f0464ad64ecea1c46718691176a13cc7
armandogb12345 <jarmandogb1@gmail.com>
2020-03-05 13:24:09 -0600

    .

---
+++ 02_Implementación/03_Componentes_Software/pro_juventud_saludable/android/app/src/main/kotlin/com/example/pro_juventud_saludable/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pro_juventud_saludable
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
