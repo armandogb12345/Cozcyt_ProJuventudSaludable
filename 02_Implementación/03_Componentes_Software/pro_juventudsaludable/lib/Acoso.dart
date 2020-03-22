58bc172fbaaaecb13ee9b05f9920d734214f85d9
armandogb12345 <jarmandogb1@gmail.com>
2020-03-21 18:40:33 -0600

    .

--- 02_Implementación/03_Componentes_Software/pro_juventudsaludable/lib/Acoso.dart
+++ 02_Implementación/03_Componentes_Software/pro_juventudsaludable/lib/Acoso.dart
@@ -13,9 +13,9 @@ class Acoso extends StatefulWidget {
 }
 class _AcosoState extends State<Acoso> {
   @override
-  Widget build(BuildContext context) {     
+  Widget build(BuildContext context) {    
     return Scaffold(
-      backgroundColor: Color(0XFFFAFAFA),
+      backgroundColor: Color(0xFFFFB74D),
       appBar: AppBar(
         leading: IconButton(
           onPressed: () {
@@ -30,6 +30,62 @@ class _AcosoState extends State<Acoso> {
         style: TextStyle(fontFamily: 'Armando',fontSize: 21.0, color: Colors.white)),
        centerTitle: true,
         ),
+    body: ListView(
+      children: <Widget>[          
+        Container(
+          child: Column(     
+              children: <Widget>[ 
+                Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,                    
+                      children: [   
+                        Expanded(
+                        child: _buildSubSistemaItem('assets/LogoLABSOL/logoLabsol.PNG'),
+                        ), 
+                      ],
+                    ) ,
+              Text('         ',
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'nombrefuente'),
+                          ),
+              Text(' INFORMACIÓN   ',
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'nombrefuente',fontSize: 25),
+                          ),
+              ],
+    ),
+        ),
+      ],),
     );
   }
+Widget _buildSubSistemaItem(String imgPath2){
+  return Padding(
+    padding: EdgeInsets.only(left:0.0, right:0.0, top: 0.0),
+    child: InkWell(
+      onTap: () { 
+        },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[          
+        Container(
+            child: Row(
+              children: [
+                Hero(
+                  tag: imgPath2,
+                  child: Image(                    
+                    image:AssetImage(imgPath2),
+                    fit: BoxFit.cover,
+                    height: 145.0,
+                    width: 360.0
+                  )
+                ),
+              ]
+            )
+          ),
+        ]
+      ),
+    ),
+  );
+}
 }
